c73cdabd1d3538834fc0acf5df8b07127be2631f
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-03-26 16:53:19 -0400

    completed all projects and stretch

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(food){
+  if(this.stomach.length < 10){
+  this.stomach.push(food);
+  }
+};
+Person.prototype.poop = function(){
+  this.stomach = [];
+};
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`
+};
+
 
 /*
   TASK 2
@@ -57,8 +73,29 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+Car.prototype.fill = function(gallons){
+  
+  this.tank+= gallons;
+}
+Car.prototype.drive = function(distance){
+  let drivable = this.tank * this.milesPerGallon;
+  if(drivable < distance){
+    this.odometer+= drivable;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }else{
+  this.odometer+= distance;
+  this.tank-= (distance/this.milesPerGallon);
+  }
 
+    
 }
 
 /*
@@ -68,17 +105,28 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  Person.call(this, favoriteToy);
+  this.favoriteToy = favoriteToy;
+  this.name = name;
+  this.age = age;
 }
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function(){
+  return (`Playing with ${this.favoriteToy}`);
+}
+
+
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
+  1. when used in a function contained in the global scope, 'this' refers to the window object. 
+  2. when a function is called after a dot, 'this' refers to the object to the left of the dot when 
+  3. in a constructor 'this' refers to  the object being created.
   4. 
 */
 
